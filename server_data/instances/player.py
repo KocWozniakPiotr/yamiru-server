e579ca2a599f88e7d0ed0d675047657da55b8ca6
KocWozniakPiotr <peter.kotsch@protonmail.com>
2023-10-26 07:31:30 +0200

    Revert "minor fixes in sql queries"
    This reverts commit a3a861bae9fe46ec2bf970c1e8a76458ac03bdf7.

--- server_data/instances/player.py
+++
@@ -1,7 +0,0 @@
-class ProfessionInstance:
-    _buster = {'type':'Buster', 'level': 1, 'hp': 100, 'atk':15, 'def': 4}
-    _maestro = {'type':'Maestro', 'level': 1, 'hp': 125, 'atk':5, 'def': 2}
-    _collector = {'type':'Collector', 'level': 1, 'hp': 75, 'atk':10, 'def': 2}
-    get_profession = [_buster, _maestro, _collector]
-
-
